8c7abe8814b0e9e52e7ecb19778b3a71858bfe75
Ziyu Liu <s7ziliuu@uni-bonn.de>
2020-06-18 20:53:03 +0200

    ex08

--- ex08/Matrix.cpp
+++ ex08/Matrix.cpp
@@ -3,23 +3,23 @@
 #include <vector>
 
 using std::cout;
-Matrix::Matrix(int rows, int columns) : _R(rows), _C(columns) {
+Matrix::Matrix(int rows,int columns) : _R(rows), _C(columns) {
   cout << "INFO::Constr. 'Matrix::Matrix(rows,columns)'\n";
   _data.resize(_R * _C);
   std::fill(_data.begin(), _data.end(), 0);
 }
 
-Matrix::Matrix(int rows, int columns, const std::vector<double> &data)
+Matrix::Matrix( int rows,int columns,const std::vector<double> &data)
     : _R(rows), _C(columns), _data(data) {
   cout << "INFO::Constr. 'Matrix::Matrix(rows,columns,data)'\n";
 }
 
-void Matrix::resize(int rows, int columns) {
+void Matrix::resize(int rows,int columns) {
   _R = rows;
   _C = columns;
 }
 
-double Matrix::get(int row, int column) {
+double Matrix::get(int row,int column) const{
   if (row > _R || column > _C) {
     cout << "The element doesn't exist!\n";
     return 0;
@@ -38,17 +38,17 @@ void Matrix::set(int row, int column, double x) {
   }
 }
 
-double Matrix::max() {
+double Matrix::max() const {
   auto maximum = std::max_element(_data.begin(), _data.end());
   return (*maximum);
 }
 
-double Matrix::min() {
+double Matrix::min() const{
   auto minimum = std::min_element(_data.begin(), _data.end());
   return (*minimum);
 }
 
-double Matrix::max(int &r, int &c) {
+double Matrix::max(const int &r,const int &c) const{
   auto maximum = std::max_element(_data.begin(), _data.end());
   auto loc = std::distance(_data.begin(), maximum);
   int R = (loc + 1) % _R;
@@ -56,7 +56,7 @@ double Matrix::max(int &r, int &c) {
   cout << "at row " << R << " column " << C << " : ";
   return (*maximum);
 }
-double Matrix::min(int &r, int &c) {
+double Matrix::min (const int &r,const  int &c) const {
   auto minimum = std::min_element(_data.begin(), _data.end());
   auto loc = std::distance(_data.begin(), minimum);
   int R = (loc + 1) % _R;
@@ -65,7 +65,7 @@ double Matrix::min(int &r, int &c) {
   return (*minimum);
 }
 
-void Matrix::print() {
+void Matrix::print() const{
   cout << "[";
   for (int i = 0; i < _R - 1; i++) {
     for (int j = 0; j < _C - 1; j++)
